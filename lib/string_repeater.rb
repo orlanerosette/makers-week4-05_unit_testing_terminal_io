1186615c6131ff0417a4f2230eea90bcad78d6b3
Orlane <orlanedoumbe@gmail.com>
2022-03-16 14:15:08 +0000

    Completed challenge

---
+++ lib/string_repeater.rb
@@ -0,0 +1,28 @@
+class StringRepeater
+  def initialize(terminal)
+    @terminal = terminal
+  end
+
+  def run
+    @terminal.puts "Hello. I will repeat a string many times."
+    @terminal.puts "Please enter a string"
+    string = @terminal.gets.chomp
+    @terminal.puts "Please enter a number of repeats"
+    number = @terminal.gets.chomp.to_i
+    @terminal.puts "Here is your result:"
+    @terminal.puts "#{string*number}"
+  end
+
+end
+
+
+# string_repeater = StringRepeater.new(Kernel)
+# string_repeater.run
+
+# Hello. I will repeat a string many times.
+# Please enter a string
+# TWIX
+# Please enter a number of repeats
+# 10
+# Here is your result:
+# TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX
