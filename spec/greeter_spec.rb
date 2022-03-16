6171bf673e7f94017a954bbfe8c6c7350c6be8c4
Orlane <orlanedoumbe@gmail.com>
2022-03-16 12:47:35 +0000

    Added .ordered

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -3,9 +3,9 @@ require 'greeter'
 RSpec.describe Greeter do
   it "asks the user their name and says hello" do
     terminal = double :terminal
-    expect(terminal).to receive(:puts).with("What is your name?")
-    expect(terminal).to receive(:gets).and_return("Orlane")
-    expect(terminal).to receive(:puts).with("Hello, Orlane!")
+    expect(terminal).to receive(:puts).with("What is your name?").ordered
+    expect(terminal).to receive(:gets).and_return("Orlane").ordered
+    expect(terminal).to receive(:puts).with("Hello, Orlane!").ordered
     greeter = Greeter.new(terminal)
     greeter.greet
   end
